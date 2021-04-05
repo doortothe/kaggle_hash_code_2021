71f77587de32eaa565b0e8c26f2cd7badfa673a2
doorto <danielroyer9@gmail.com>
2021-04-05 08:16:56 -0700

    Got started on the run_simulation() method. Debugging is pain.

--- classes.py
+++ classes.py
@@ -54,11 +54,15 @@ class Simulation:
         self.num_cars = validate_variable(num_cars, 'S')
         self.points = validate_variable(points, 'F')
 
+        # Simulation variables
         self.score = 0
         self.current_time = 0
         self.streets = []
         self.cars = []
         self.intersections = []
+
+        # Statistic tracking variables
+        self.num_cars_score_before_end = 0
 
     @property
     def get_num_streets(self):
@@ -108,14 +112,14 @@ class Simulation:
         # Create the streets
         streets = []
         intersections = []
-        for x in range(0, sim.get_num_streets):
+        for x in range(sim.get_num_streets):
             line = f.readline().split(' ')
             streets.append(Street(line[2], line[0], line[1], line[3]))
 
             # Add the new intersection to the list, or add new street to existing intersection
             # Check if current intersection is already in the list
             # todo: rewrite code to look/add new intersections in both line[0] and line[1]
-            i = find(intersections, line[1])
+            i = find(intersections, int(line[1]))
             if i > -1:
                 # Check if current street is already in the intersection's list
                 if line[2] not in intersections[i].get_streets:
@@ -133,8 +137,9 @@ class Simulation:
 
         # Create the cars
         cars = []
-        for y in range(0, sim.get_num_cars):
-            line = f.readline().replace('\n', '').split(' ')
+        for y in range(sim.get_num_cars):
+            line = f.readline().replace('\n', '').split(
+                ' ')  # Need to remove new line character to avoid bug when finding unique streets
             cars.append(Car(line[0], line[1:]))
 
         # store cars in the simulation's car array
@@ -144,6 +149,7 @@ class Simulation:
         f.close()
         return sim
 
+    # todo: combine read_input and read_submission so they are called in one line by main.py
     def read_submission(self, file):
         f = open(file, 'r')
         num_schedules = int(f.readline())
@@ -151,8 +157,9 @@ class Simulation:
         # Read through the file as specified by the first line
         for x in range(num_schedules):
             # This line tell us the intersection we're working with
-            intersection = f.readline()
 
+            intersection = int(f.readline())
+            # print("Working on intersection " + intersection)
             # The next line tells us how many lights we're looking at
             num_lights = int(f.readline())
 
@@ -166,15 +173,21 @@ class Simulation:
 
                 # todo: add documentation describing what line parts are what
                 schedule['street'] = line[0]
+                # todo: add timer variable constraints (1 <= timer <= duration)
                 schedule['timer'] = int(line[1])
                 schedules.append(schedule)
 
             # Give the schedule to the appropriate intersection
             # todo: add try-except statement in case unable to find intersection for whatever reason
             self.intersections[find(self.intersections, intersection)].set_schedule(pd.DataFrame(schedules))
+            #print(intersection)
 
         #  Finally, cut all unnecessary streets and intersections, where no car will interact with them
         self.cut_streets()
+
+        # for i in self.intersections:
+        #     print("id: " + i.id)
+        #     print('streets: ' + str(i.get_streets))
 
     def create_submission(self, file=None):
         # todo: implement a version for creating a flat submission file for a new input file
@@ -185,30 +198,6 @@ class Simulation:
         """
         pass
 
-    def score(self, car):
-        # calculate score based on car parameters
-        if self.current_time <= self.duration:  # If the car was removed before the simulation ended
-            self.score += self.points + (self.duration - self.current_time)
-        # Otherwise, no score is added
-
-        # todo: Remove car from the simulation
-
-        # todo: record car stats
-        pass
-
-    def tick(self):
-        # Update traffic light schedules
-        for i in self.intersections:
-            pass
-
-        # Move cars/count delay
-        for i in self.streets:
-            pass
-
-        # Check if car reached its destination
-        pass
-
-    # todo: find and cut unused streets/intersections to reduce number of features in optimization
     def cut_streets(self):
         # Create list of all streets and intersections cars touch
         all_streets = pd.DataFrame()
@@ -240,17 +229,86 @@ class Simulation:
         [self.intersections.remove(it) for it in self.intersections if it.get_id not in unique_intersections]
         # print(self.intersections)
 
-    # todo: implement optimization method
+    def run_simulation(self):
+        # for i in self.intersections:
+        #     print(i.get_id)
+        #     print(i.get_schedule)
+
+        # Find lights that are always set to on/off and mark as such to optimize later ticks
+        for i in self.intersections:
+            # A light is always on if only one light is mentioned in a schedule
+            if i.get_schedule is None:
+                # How do I update both the intersection and the street?
+                print("Removing intersection " + str(i.get_id) + " because its always red.")
+                self.intersections.remove(i)
+
+            # A light is always off if the intersection is not listed in the schedule
+            elif i.get_schedule.shape[0] == 1:
+
+                # Street to update
+                self.streets[find(self.streets, i.get_schedule['street'][0])].flip_state()
+
+                print("Removing intersection " + str(i.get_id) + " because its always green.")
+
+                # Remove light from list of intersections to check each tick
+                self.intersections.remove(i)
+        # print(str(self.intersections))
+        # Simulation loop
+
+        for i in self.intersections:
+            print(i.get_id)
+            print(i.get_schedule)
+
+        for self.current_time in range(self.duration):
+            # todo: implement in-simulation print/record statements.
+            self.tick
+
+            # Check if cars reached their destinations
+
+            # Check statistics
+
+    def tick(self):
+        # Update traffic light schedules
+        for i in self.intersections:
+            # Check which light should be on
+            pass
+
+        # Move cars/count delay
+        for i in self.streets:
+            pass
+
+        pass
+
+    def score(self, car):
+        # calculate score based on car parameters
+        if self.current_time <= self.duration:  # If the car was removed before the simulation ended
+            self.score += self.points + (self.duration - self.current_time)
+            self.num_cars_score_before_end += 1
+        # Otherwise, no score is added
+
+        # todo: record car stats
+        """
+        Stats to track:
+            When car was removed
+            Points earned
+            Time spent at red light
+        """
+
+        # todo: Remove car from the simulation
+        self.cars.remove(car)
+
+    # todo: implement optimization method/class
 
     # todo: implement method to find statistics. Such as:
     """
     amount(%) of cars that arrived before deadline.
-    Earlist arrival (points earned, time driven)
+    Earliest arrival (points earned, time driven)
     Latest arrival (and points earned, time driven)
     Average points earned
     Average time(ticks) driven
     Create graph/database of tracking positions and num lights at a time
     Create visualizations of such
+    Amount of delay caused by each street per tick
     """
 
 
@@ -308,19 +366,24 @@ class Intersection:
     GREEN = 1
 
     def __init__(self, name, street):
-        self.id = name
+        self.id = int(name)
         self.streets = [street]  # List of street ids of streets that meet at this intersection
-        self.schedule = pd.DataFrame()
-        # self.cycle_length
+
+        # Schedule variables
+        self.light = self.RED
+        self.always_on = False
+        self.always_off = False
         self.cycle_position = 0
+        self.schedule = None
+
+        # Statistic variables
         self.delay = 0
 
     def set_schedule(self, schedule):
         self.schedule = schedule
 
-        # Calculate the 'hidden_timer' column for each row
+        # Calculate the 'hidden_timer' as a cumulative sum
         self.schedule['hidden_timer'] = self.schedule['timer'].cumsum()
-        self.schedule['Light'] = self.RED
 
         # print(self.schedule)
 
@@ -342,11 +405,36 @@ class Intersection:
     def get_schedule_position(self):
         return self.cycle_position
 
+    @property
+    def get_schedule(self):
+        return self.schedule
+
+    def set_always_on(self):
+        # todo: add check/security so that always_on/always_off can't both be on
+        """
+        Should only be called in the first tick of a simulation
+        :return:
+        """
+
+        self.always_on = True
+        self.light = self.GREEN
+
+    def set_always_off(self):
+        # todo: add check/security so that always_on/always_off can't both be on
+        """
+        Should only be called in the first tick of a simulation
+        :return:
+        """
+        self.always_off = True
+        # light is off by default, so no need to change it
+
     def __repr__(self):
         return self.id
 
 
 class Street:
+    RED = 0
+    GREEN = 1
 
     def __init__(self, name, start_intersection_id, end_intersection_id, length):
         self.id = name
@@ -354,8 +442,10 @@ class Street:
         self.positions = [None] * self.length  # Initialize as empty street
         self.start_intersection_id = start_intersection_id
         self.end_intersection_id = end_intersection_id
-        self.light = TrafficLight(self.id)
         self.street_delay = 0
+        self.state = self.RED
+
+        # todo: simplify code so updating a traffic light in intersection will also update its corresponding street
 
     def place_car(self, car):
         """
@@ -380,6 +470,10 @@ class Street:
     def get_intersection(self):
         return self.end_intersection_id
 
+    @property
+    def get_state(self):
+        return self.state
+
     def __repr__(self):
         return self.id
 
@@ -387,3 +481,9 @@ class Street:
     def add_delay(self):
         # Count number of cars in positions[] array and add 1 delay for each
         pass
+
+    def flip_state(self):
+        if self.state == self.RED:
+            self.state = self.GREEN
+        else:
+            self.state = self.RED
--- main.py
+++ main.py
@@ -7,3 +7,8 @@ from classes import Simulation
 
 sim = Simulation.read_input('test_sim.txt')
 sim.read_submission('test_sched.txt')
+
+# Run simulation
+sim.run_simulation()
+# Expected outcome: there should only be 1 intersections: 1
+# Intersections 0 and 2 are always green
